e51adf42e658c15939d528d57ce842ab3e7e708b
Michael Bubb <mbubb@paperlesspost.com>
2021-06-22 17:22:27 -0400

    add flag

--- filecheck.go
+++ filecheck.go
@@ -1,24 +1,40 @@
 package main
 
 import (
-  "fmt"
-  "flag"
-	"errors"
-	"io"
+	"flag"
+	"fmt"
 	"os"
+	"path"
 	"path/filepath"
-	"strings"
 )
 
+func main() {
+	pathPtr := flag.String("basepath", "/Users/mbubb/git/terraforming", "string - path to search")
+	flag.Parse()
+	
+	fmt.Printf(*pathPtr)
 
-err := filepath.Walk("/Users/mbubb/git/terraforming/projects",
-    func(path string, info os.FileInfo, err error) error {
-    if err != nil {
-        return err
-    }
-    fmt.Println(path, info.Size())
-    return nil
-})
-if err != nil {
-    log.Println(err)
+	list := getTerraScript(*pathPtr)
+	for i, p := range list {
+		fmt.Printf("[%d:%s===%s]\n", i, path.Dir(p), path.Base(p))
+	}
+}
+
+func getTerraScript(rootpath string) []string {
+
+	list := make([]string, 0, 10)
+
+	err := filepath.Walk(rootpath, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if filepath.Ext(path) == ".tf" {
+			list = append(list, path)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("walk error [%v]\n", err)
+	}
+	return list
 }
